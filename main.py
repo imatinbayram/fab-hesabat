274b5024ca39a575985cd21b8dc66f2ce3c9f4cd
Matin Bayram <imatinbayram@gmail.com>
2025-10-20 19:11:37 +0400

    Create main.py

---
+++ main.py
@@ -0,0 +1,16 @@
+import streamlit as st
+
+# Define pages
+pages = {
+    "Günlük hesabat": "Hesabat.py",
+    "ŞOK Kampaniya": "Cesit.py"
+}
+
+# Streamlit's new navigation API
+selected_page = st.radio("Menyu", list(pages.keys()), horizontal=True)
+
+# Run the selected page
+if selected_page == "Günlük hesabat":
+    import Hesabat
+elif selected_page == "ŞOK Kampaniya":
+    import Cesit
